1a9adc3f84a711960efcf085808868d781379b91
Niklaaus <saurabhsrivastav1990@gmail.com>
2019-06-19 00:13:37 +0530

    added endpoints and services for creation of collections and adding faces to collection

--- src/main/java/com/recognizr/rekognition_aws/controller/RekognitionController.java
+++ src/main/java/com/recognizr/rekognition_aws/controller/RekognitionController.java
@@ -1,13 +1,18 @@
 package com.recognizr.rekognition_aws.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.recognizr.rekognition_aws.service.RekognitionService;
@@ -15,22 +20,72 @@ import com.recognizr.rekognition_aws.service.RekognitionService;
 @RestController
 public class RekognitionController {
 
-@Autowired
-RekognitionService rekogService;
+	@Autowired
+	RekognitionService rekogService;
 
-@GetMapping(value="/test", produces="application/json")
-public ResponseEntity testingRekognition() {
-	String filepath="/media/chilgoza/Photos & Videos/Personal Photos n Videos/VaultOfPhotos101/IMG-20150530-WA0003.jpg";
-	byte[] bytes;
-    try {
-        bytes = Files.readAllBytes(Paths.get(filepath));
-    } catch (IOException e) {
-        System.err.println("Failed to load image: " + e.getMessage());
-        return new ResponseEntity(null,HttpStatus.HTTP_VERSION_NOT_SUPPORTED);
-    }
-   
-	return new ResponseEntity(rekogService.detectLabels(bytes),HttpStatus.OK);
+	@GetMapping(value = "/test", produces = "application/json")
+	public ResponseEntity testingRekognition() {
+		String filepath = "/media/chilgoza/Photos & Videos/Personal Photos n Videos/VaultOfPhotos101/IMG-20150530-WA0003.jpg";
+		byte[] bytes;
+		try {
+			bytes = Files.readAllBytes(Paths.get(filepath));
+		} catch (IOException e) {
+			System.err.println("Failed to load image: " + e.getMessage());
+			return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+		}
+		try {
+			return new ResponseEntity(rekogService.detectLabels(bytes), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@PostMapping(value = "/create-collection", produces = "application/json")
+	public ResponseEntity createIfNotExistsCollection(@RequestBody String collectionName) {
+
+		try {
+			if (!StringUtils.isEmpty(collectionName)) {
+				rekogService.createIfNotExistsCollection(collectionName);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+
+		return new ResponseEntity(HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/add-face", produces = "application/json")
+	public ResponseEntity addFacesToCollection(@RequestBody String collectionName) { // will add another parameter to
+																						// handle base64 image coming
+																						// from front end
+
+		if (!rekogService.collectionExists(collectionName)) {
+			System.out.println("collection of this name does not exist");
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+
+		// temp code till front end is not created for training
+		String folderPath = "/home/chilgoza/Downloads/" + collectionName;
+		File foldername = new File(folderPath);
+		for (File f : foldername.listFiles()) {
+			Path path = Paths.get(f.getAbsolutePath());
+			try {
+				byte[] bytes = Files.readAllBytes(path);
+				rekogService.addFaceToCollection(collectionName, bytes);
+			} catch (Exception e) {
+				return new ResponseEntity(HttpStatus.BAD_REQUEST);
+			}
+
+		}
+		// temp code till front end is not created for training ends
+
+		return new ResponseEntity(HttpStatus.OK);
+	}
 	
-}
+	@PostMapping(value = "/recognize", produces = "application/json")
+	public ResponseEntity searchFace(@RequestBody String base64InputImage) {
+		
+		return null;
+	}
 	
 }
--- src/main/java/com/recognizr/rekognition_aws/service/RekognitionService.java
+++ src/main/java/com/recognizr/rekognition_aws/service/RekognitionService.java
@@ -5,20 +5,29 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.CreateCollectionRequest;
+import com.amazonaws.services.rekognition.model.CreateCollectionResult;
 import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
 import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Face;
+import com.amazonaws.services.rekognition.model.FaceDetail;
+import com.amazonaws.services.rekognition.model.FaceRecord;
 import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.IndexFacesRequest;
+import com.amazonaws.services.rekognition.model.IndexFacesResult;
 import com.amazonaws.services.rekognition.model.Label;
+import com.amazonaws.services.rekognition.model.ListCollectionsRequest;
+import com.amazonaws.services.rekognition.model.ListCollectionsResult;
 import com.recognizr.rekognition_aws.factory.ClientFactory;
 
 @Service
 public class RekognitionService {
 
-	public Map<String, Float> detectLabels(byte[] bytes) {
+	public Map<String, Float> detectLabels(byte[] bytes) throws Exception {
 		Map<String, Float> responseLabels = new HashMap<String, Float>();
 		ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
 
@@ -34,6 +43,73 @@ public class RekognitionService {
 			responseLabels.put(label.getName(), label.getConfidence());
 		}
 		return responseLabels;
+	}
+
+	public void createIfNotExistsCollection(String collectionName) throws Exception {
+		
+		if (!collectionExists(collectionName)) {
+		AmazonRekognition rekognition = ClientFactory.createClient();
+		 CreateCollectionRequest request = new CreateCollectionRequest()
+	                .withCollectionId(collectionName);
+	 
+	        CreateCollectionResult resultCollection = rekognition.createCollection(request);
+	 
+	        Integer statusCode = resultCollection.getStatusCode();
+	        String collectionArn = resultCollection.getCollectionArn();
+	        String faceModelVersion = resultCollection.getFaceModelVersion();
+	        
+	        if(statusCode!=200 || StringUtils.isEmpty(collectionArn) || StringUtils.isEmpty(faceModelVersion)) {
+	        	throw new Exception("some error creating collection" + collectionName);
+	        }
+	}
+	}
+	
+	public boolean collectionExists(String collectionName) {
+		ListCollectionsRequest listRequest = new ListCollectionsRequest().withMaxResults(100);
+
+		AmazonRekognition rekognition = ClientFactory.createClient();
+		ListCollectionsResult resultList = rekognition.listCollections(listRequest);
+
+		List<String> collectionIds = resultList.getCollectionIds();
+		while (collectionIds != null) {
+			if (collectionIds.contains(collectionName)) {
+				System.out.println("collection with this name already exists");
+				return true;
+			}
+
+			String token = resultList.getNextToken();
+			if (token != null) {
+				resultList = rekognition.listCollections(listRequest.withNextToken(token));
+			} else {
+				collectionIds = null;
+			}
+		}
+		
+		return false;
+	}
+
+	public void addFaceToCollection(String collectionName, byte[] bytes) {
+		ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+
+		AmazonRekognition rekognition = ClientFactory.createClient();
+		  IndexFacesRequest request = new IndexFacesRequest()
+                  .withCollectionId(collectionName)
+                  .withDetectionAttributes("ALL")
+                  .withImage(new Image().withBytes(byteBuffer));
+          IndexFacesResult result = rekognition.indexFaces(request);
+		//debug code
+
+          List<FaceRecord> faceRecords = result.getFaceRecords();
+          for (FaceRecord rec : faceRecords) {
+              FaceDetail faceDetail = rec.getFaceDetail();
+             
+              Face face = rec.getFace();
+              System.out.println("Face-ID: " + face.getFaceId() +
+                      "\nImage ID: " + face.getImageId() +
+                      "\nExternal Image ID: " + face.getExternalImageId() +
+                      "\nConfidence: " + face.getConfidence());
+          }
+          //debug ends
 	}
 
 }
